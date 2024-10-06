d7061941e5acb20065753ea0891226d5794a959a
chmzs <66004504+chmzs@users.noreply.github.com>
2024-10-06 21:01:06 +0800

    Delete R/logo.R
    shanchu

--- R/logo.R
+++
@@ -1,8 +0,0 @@
-#' Bibliometrix logo.
-#'
-#' The matrix contains the rgb format of the bibliometrix official logo.\cr
-#'
-#' @format A matrix with 927 rows and 800 columns.
-#' 
-#' @name logo
-NULL
